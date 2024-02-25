100eb117b4509f62418ca7df4d0900a0d7a71c47
Rahul <kr.rahul79@gmail.com>
2024-02-25 21:37:14 +0000

    Reverting middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // Set the paths that don't require the user to be signed in
-  // Sign in and sign up pages are already made public by Clerk
-  publicRoutes: ["/"],
+  publicRoutes: ["/api/:path*"],
 });
 
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico|.*.svg).*)",
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
