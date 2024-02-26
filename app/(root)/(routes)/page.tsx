a3d690632a6ca2b65c4e69234d3de499bd1c2e96
Rahul <kr.rahul79@gmail.com>
2024-02-26 21:26:42 +0000

    Removing the db call

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,6 @@
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 import Link from "next/link";
-import prismadb from "@/lib/prismadb";
 
 export default async function Page() {
-  const store = await prismadb.category.findFirst({
-    where: {
-      storeId: "2553e45e-55c7-424f-8a15-fa48e566509d",
-    },
-  });
-
   return <Link href="/2553e45e-55c7-424f-8a15-fa48e566509d">Dashboard</Link>;
 }
